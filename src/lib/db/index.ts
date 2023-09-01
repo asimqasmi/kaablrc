226e7950f02efca640dda31cd536215dd51b19be
asimqasmi <kaabteacher@gmail.com>
2023-09-01 19:29:13 +0400

    edit schema

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,7 +3,13 @@ import { drizzle } from "drizzle-orm/node-postgres"
 import { Client } from "pg"
 
 import * as dotenv from "dotenv"
-import { users } from "./schema"
+import { users } from "./schema/user"
+import { books } from "./schema/book"
+import { bookings } from "./schema/booking"
+import { borrowings } from "./schema/borrowing"
+import { lessons } from "./schema/lesson"
+import { likes } from "./schema/like"
+import { replies } from "./schema/reply"
 
 dotenv.config()
 
@@ -15,4 +21,14 @@ const client = new Client({
 
 client.connect()
 
-export const db = drizzle(client, { schema: { ...users } })
+export const db = drizzle(client, {
+  schema: {
+    ...users,
+    ...books,
+    ...bookings,
+    ...borrowings,
+    ...lessons,
+    ...likes,
+    ...replies,
+  },
+})
